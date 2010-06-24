ed253723378e612c2cd8e4f292564b24748e222d
Ethan Shepherd <someguy235@gmail.com>
2010-06-23 21:12:20 -0400

    added springSecurity --ems

---
+++ grails-app/conf/SecurityConfig.groovy
@@ -0,0 +1,35 @@
+security {
+
+	// see DefaultSecurityConfig.groovy for all settable/overridable properties
+
+	active = true
+	cacheUsers = false
+
+	loginUserDomainClass = "com.friendlibrary.User"
+	authorityDomainClass = "com.friendlibrary.Role"
+	//requestMapClass = "Requestmap"
+		
+	userName = "username"
+	password = "password"
+		
+	useRequestMapDomainClass = false
+	
+	requestMapString = """\
+	CONVERT_URL_TO_LOWERCASE_BEFORE_COMPARISON
+	PATTERN_TYPE_APACHE_ANT
+	/=IS_AUTHENTICATED_ANONYMOUSLY
+	/login/auth=IS_AUTHENTICATED_ANONYMOUSLY
+	/login/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/item/list=IS_AUTHENTICATED_ANONYMOUSLY
+	/item/create=IS_AUTHENTICATED_ANONYMOUSLY
+	/user/list=IS_AUTHENTICATED_ANONYMOUSLY
+	/user/create=IS_AUTHENTICATED_ANONYMOUSLY
+	/user/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/profile/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/js/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/css/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/images/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/plugins/**=IS_AUTHENTICATED_ANONYMOUSLY
+	/**=IS_AUTHENTICATED_REMEMBERED
+	"""
+}
